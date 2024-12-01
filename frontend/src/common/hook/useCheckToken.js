4c38ebdeeb1f421c7d8416ea60054d55106cacff
Dong <k30110106@gmail.com>
2024-12-01 15:38:08 +0900

    토큰검사 리액트 커스텀 훅 수정

--- frontend/src/common/hook/useCheckToken.js
+++ frontend/src/common/hook/useCheckToken.js
@@ -1,20 +1,19 @@
 import axios from "axios";
-import { useNavigate } from "react-router-dom";
 
 /**
- * 토큰 조회 커스텀 훅 (수정중)
- * >> 비로그인 상태에서 접근 시 강제로 로그인페이지 이동처리
+ * 토큰 조회 및 재발급 커스텀 훅 (수정중)
+ * >> 비로그인 상태에서 접근 시 강제로 로그인페이지 이동처리 (취소)
+ * >> true, false 리턴 처리로 변경
  *
  * 1. 엑세스 토큰 조회 (postAccessTokenApi)
- *  1-1. 정상 -> PASS
+ *  1-1. 정상 -> true
  *  1-2. 실패 -> 리프레시 토큰 조회 + 엑세스 토큰 재발급 시도 (postRefreshTokenApi)
- *      1-2-1. 정상 -> PASS
- *      1-2-2. 실패 -> 로그인페이지 이동
+ *      1-2-1. 정상 -> true
+ *      1-2-2. 실패 -> 로그인페이지 이동(취소) -> false + 엑세스 토큰 로컬스토리지 삭제
  * */
 const API_BASE_URL = process.env.REACT_APP_API_ROOT;
 
 export const useCheckToken = () => {
-  const navigate = useNavigate();
 
   const postAccessTokenApi = async () => {
     const accessToken = localStorage.getItem("access_token");
@@ -22,7 +21,19 @@ export const useCheckToken = () => {
       headers: { Authorization: `Bearer ${accessToken}` },
     };
 
-    return await axios.post(`${API_BASE_URL}/api/token/access`, null, config);
+    try {
+      const response = await axios.post(`${API_BASE_URL}/api/token/access`, null, config);
+      if (response.status === 200 && response.data.result) {
+        console.log("엑세스 토큰 인증 성공");
+        return true;
+      }
+
+      console.log("엑세스 토큰 인증 실패");
+      return false;
+    } catch (err) {
+      console.error("엑세스 토큰 실패:", err);
+      return false;
+    }
   };
 
   const postRefreshTokenApi = async () => {
@@ -30,37 +41,35 @@ export const useCheckToken = () => {
       withCredentials: true,
     };
 
-    await axios
-      .post(`${API_BASE_URL}/api/token/refresh`, null, config)
-      .then((res) => {
-        //console.log(res);
-        if (res.status !== 200) {
-          window.alert("토큰 재발급 실패! 로그인페이지 이동!");
-          navigate({ pathname: "/login" }, { replace: true });
-          return;
-        }
+    try {
+      const response = await axios.post(`${API_BASE_URL}/api/token/refresh`, null, config);
+
+      if (response.status === 200 && response.data.accessToken) {
+        // 새로운 엑세스 토큰을 로컬 스토리지에 저장
+        localStorage.setItem("access_token", response.data.accessToken);
         console.log("토큰 재발급 성공");
-        localStorage.setItem("access_token", res.data.accessToken);
-      })
-      .catch((err) => {
-        //console.log(err);
-        window.alert("토큰 재발급 실패! 로그인페이지 이동!");
-        navigate({ pathname: "/login" }, { replace: true });
-      });
+        return true;
+      }
+
+      console.log("토큰 재발급 실패");
+      return false;
+    } catch (err) {
+      console.error("리프레시 토큰 실패:", err);
+      return false;
+    }
   };
 
   const checkToken = async () => {
-    try {
-      const response = await postAccessTokenApi();
-      if (response.status !== 200) {
-        await postRefreshTokenApi();
-        return;
-      }
-      console.log("토큰 인증 성공");
-    } catch (e) {
-      console.error(e);
-      await postRefreshTokenApi();
+    if(await postAccessTokenApi()) {
+       return true;
     }
+
+    if(await postRefreshTokenApi()) {
+      return true;
+    }
+
+    localStorage.removeItem("access_token");
+    return false;
   };
 
   return { checkToken };
