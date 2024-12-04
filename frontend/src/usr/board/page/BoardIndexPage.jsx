5b66f48eeae030aa82ecf36c62b274dd366a1a55
JKuna7 <97491259+JKuna7@users.noreply.github.com>
2024-12-04 17:00:21 +0900

    boardindex -> board 링크 수정

--- frontend/src/usr/board/page/BoardIndexPage.jsx
+++ frontend/src/usr/board/page/BoardIndexPage.jsx
@@ -7,8 +7,8 @@ function BoardIndexPage() {
             <h3>
                 여기는 BoardIndex 입니다.
                 <h1>
-                    <Link to="boardlist">[BoardList로 이동하기]</Link>
-                    <Link to="boardcreate">[글쓰기]</Link>
+                    <Link to="list">[BoardList로 이동하기]</Link>
+                    <Link to="create">[글쓰기]</Link>
                 </h1>
             </h3>
             {/* Outlet: 여기에 하위 페이지가 렌더링됨 */}
