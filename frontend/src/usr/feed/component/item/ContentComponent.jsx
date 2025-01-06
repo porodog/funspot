09a994555d79ae7c15738f7ac2d10f20d3171a34
k30110106 <k30110106@gmail.com>
2025-01-06 15:17:46 +0900

    해시태그 모달 디자인 수정

--- frontend/src/usr/feed/component/item/ContentComponent.jsx
+++ frontend/src/usr/feed/component/item/ContentComponent.jsx
@@ -13,12 +13,12 @@ const ContentComponent = ({ feed, openDetailModal }) => {
       >
         <span className="font-semibold text-gray-800">{content}</span>
 
-        <div className="flex space-x-3">
+        <div className="flex space-x-3 flex-wrap">
           {/* 해시태그 */}
           {feedHashtags != null &&
             (feedHashtags ?? []).length > 0 &&
             feedHashtags.map((tag, index) => (
-              <span key={tag.idx} className="text-black font-bold text-lg">
+              <span key={tag.idx} className="text-black font-bold text-lg mb-1">
                 #{tag.tagName}
               </span>
             ))}
