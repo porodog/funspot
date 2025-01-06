620480c2351e7f58f0507072658cfb41f4639f25
jklasd1472 <jklasd1472@gmail.com>
2025-01-06 17:39:03 +0900

    뒤로가기 버튼 생성

--- frontend/src/usr/feed/page/IndexPage.jsx
+++ frontend/src/usr/feed/page/IndexPage.jsx
@@ -1,9 +1,11 @@
+import GoBackButton from "../../../common/hook/useBackbutton";
 import BasicLayout from "../../../common/layout/BasicLayout";
 import { Outlet } from "react-router-dom";
 
 const IndexPage = () => {
   return (
     <BasicLayout>
+      <GoBackButton />
       <div className="w-full h-auto">
         <Outlet />
       </div>
